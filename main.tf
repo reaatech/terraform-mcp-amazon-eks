1bc84bb4c60827107d2538b37969d8a18c1efdeb
reaatech <138725666+reaatech@users.noreply.github.com>
2026-04-28 16:27:56 -0700

    fix: update helm provider kubernetes block syntax for v3 compatibility

--- main.tf
+++ main.tf
@@ -145,7 +145,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
 
